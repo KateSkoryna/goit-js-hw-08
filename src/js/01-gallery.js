ea95937a5421a0bf81b1fa46f818066847e60dec
Kateryna Skoryna <k.skoryna@gmail.com>
2022-07-21 09:50:21 +0200

    Made task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const createGalleryListMarkup = gallery =>
+  gallery
+    .map(
+      ({ description, original, preview }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </div>`
+    )
+    .join('');
+
+const galleryItemsList = createGalleryListMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsList);
+
+const imageBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  animationSpeed: 250,
+});
