5d462e2eefc9a5c35aae715fb3d31b8938bf6528
Kateryna Skoryna <k.skoryna@gmail.com>
2022-07-22 13:19:34 +0200

    Added 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
+const formData = {};
+
+emailEl.addEventListener('input', throttle(getInputValues, 500));
+messageEl.addEventListener('input', throttle(getInputValues, 500));
+
+function getInputValues(event) {
+  event.preventDefault();
+
+  const name = event.target.name;
+  const value = event.target.value;
+
+  formData[name] = value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+form.addEventListener('submit', sendFormData);
+
+function sendFormData(event) {
+  event.preventDefault();
+
+  const email = event.target.elements.email.value;
+  const message = event.target.elements.message.value;
+
+  if (email === '' || message === '') {
+    alert('Please fill in all the fields!');
+  } else {
+    localStorage.removeItem('feedback-form-state');
+    console.log({ email, message });
+  }
+
+  event.currentTarget.reset();
+}
+
+function checkLocaleStorage(event) {
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (savedData.email) {
+    emailEl.value = savedData.email;
+  }
+  if (savedData.message) {
+    messageEl.value = savedData.message;
+  }
+}
+
+checkLocaleStorage();
